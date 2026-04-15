4eaab52b0ebe72ff12140b8d0110043340c95307
Eliud2023 <eliudsign@gmail.com>
2026-04-15 15:38:12 -0600

    test again remembering

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int capturaMenu;
+        boolean encendido = true;
+        String nombreCliente;
+        double saldo = 0;
+
+        Scanner ingresaDatos = new Scanner(System.in);
+
+        System.out.println("Jorge Eliud de Jesús Flores Hernández");
+
+    }
+}
+
