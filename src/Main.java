e14baa238e53ec89ac2f6a685d6153daca984880
Eliud2023 <eliudsign@gmail.com>
2026-04-17 03:58:45 -0600

    Volví a hacer el ejercicio para practicar y lo que me falló fue el while, también el agregar la variable para los pesos ingresados o retirados, también que el while no debe tener asignación = para que se termine el menú al salir, también cómo definir qué es lo que se ingresa del teclado en el caso 2 y 3... En síntesis sí hice bien el principio y se mostraba el menú, pero tuve olvidos al hacer los cases.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,57 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         int capturaMenu;
         boolean encendido = true;
         String nombreCliente;
         double saldo = 0;
-
         Scanner ingresaDatos = new Scanner(System.in);
+        int pesos;
 
-        System.out.println("Jorge Eliud de Jesús Flores Hernández");
+        while (encendido) {
+
+            System.out.println("**** MENÚ BANCARIO ****");
+            System.out.println("1. SALDO");
+            System.out.println("2. DEPÓSITO");
+            System.out.println("3. RETIRO");
+            System.out.println("4. SALIR");
+            System.out.print("Elige una opción (1-4): ");
+
+            capturaMenu = ingresaDatos.nextInt();
+
+            switch (capturaMenu) {
+
+                case 1 -> {
+                    System.out.println("Tu saldo es:$ " + saldo + "\n\n");
+                }
+                case 2 -> {
+
+                    System.out.print("Ingresa la cantidad:$ ");
+                    pesos = ingresaDatos.nextInt();
+                    saldo += pesos;
+                    System.out.println("Confirmo que ingresaste:$ " + pesos);
+                }
+                case 3 -> {
+                    System.out.print("¿Cuánto retiras?:$ ");
+                    pesos = ingresaDatos.nextInt();
+                    saldo -= pesos;
+                    System.out.println("Confirmo que retiraste:$ " + pesos);
+                }
+                case 4 -> {
+
+                    System.out.println("Hasta luego Eliud");
+                    encendido = false;
+                }
+                default -> System.out.println("Opción inválida (Elige del 1 al 4): ");
+
+            }
+
+        }
+
+        //System.out.println("Jorge Eliud de Jesús Flores Hernández");
 
     }
 }
